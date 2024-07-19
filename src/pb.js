1825e6210e0b90bcc012a9630a2775b435482a23
Eric M. G. <ericbro29@gmail.com>
2024-07-19 11:23:58 +0300

    Add Style 3 Hero

--- src/pb.js
+++ src/pb.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
-const landingPageId = '9mu0n7loxnjgqn6'
+const landingPageId = 'fawn632eqanofhl'
 
 const getImageUrl = ({ collection, filename }) => pb.files.getUrl(collection, filename)
 
